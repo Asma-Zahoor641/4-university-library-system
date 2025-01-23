8196477adee326d619237b58319ff97326e63c03
Asma Zahoor <145072181+Asma-Zahoor641@users.noreply.github.com>
2025-01-24 02:08:46 +0500

    implement   Qstash Email workflow

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,11 +3,33 @@ import Header from '@/components/Header'
 import React from 'react'
 import {auth} from "@/auth"
 import { redirect } from 'next/navigation';
+import { after } from "next/server";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const layout = async ({children} : {children: React.ReactNode}) => {
 
     const session = await auth();
    if(!session) redirect ("/sign-in");
+
+   after(async () => {
+    if (!session?.user?.id) return;
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
 
     
   return (
